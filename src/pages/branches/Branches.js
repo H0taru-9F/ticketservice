cb2076ecedf56df5dd7f2f1b5e19d022721b37e1
Vasya <feniak2003@gmail.com>
2023-12-28 10:44:27 +0200

    fix

---
+++ src/pages/branches/Branches.js
@@ -0,0 +1,18 @@
+import './branches.css';
+import React, {useEffect, useState} from "react";
+import Header from "../../components/Header";
+import Footer from "../../components/Footer";
+
+const Branches = () => {
+
+    return(
+        <div>
+            <Header/>
+            <div>
+
+            </div>
+            <Footer/>
+        </div>
+    )
+}
+export default Branches;
